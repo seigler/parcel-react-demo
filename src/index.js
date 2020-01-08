24055f27ea5c582a69809ec7fa0e303a9ce5fb99
Joshua Seigler <joshua@seigler.net>
2020-01-07 19:44:13 -0500

    feat: fetch randomusers and display them

--- src/index.js
+++ src/index.js
@@ -3,11 +3,34 @@ import ReactDOM from 'react-dom';
 
 function App ({ numContacts }) {
   const [contacts, setContacts] = useState();
+  const [status, setStatus] = useState('loading');
   if (contacts === undefined) {
-    setContacts(['a']);
-    return <div className='loading'>Loading</div>;
+    window.fetch('https://randomuser.me/api/?results=20&nat=us,cn')
+      .then(response => response.json())
+      .then(json => setContacts(json.results))
+      .catch(error => {
+        console.error(error);
+        setStatus('error');
+      });
+    return (
+      <div className={status}>
+        {
+          {
+            loading: 'Loading...',
+            error: 'Something has gone wrong.'
+          }[status]
+        }
+      </div>
+    );
   } else {
-    const list = contacts.map(c => <div key={c}>{c}</div>);
+    console.log(contacts);
+    const list = contacts.map(c =>
+      <div key={c.login.uuid}>
+        {
+          c.name.first + ' ' + c.name.last
+        }
+      </div>
+    );
     return list;
   }
 }
